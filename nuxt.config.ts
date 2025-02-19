d60112bdaa191a7da772970a5bf206aaf4f2fe84
smw0807 <smw0807@gmail.com>
2025-02-19 10:49:43 +0900

    원상복귀

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
-  // app: {
-  //   baseURL: './', // GitHub Pages를 위한 상대 경로
-  //   buildAssetsDir: 'assets', // _nuxt 대신 assets로 변경
-  // },
+  ssr: true,
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
@@ -17,13 +13,5 @@ export default defineNuxtConfig({
     options: {
       hashMode: true,
     },
-  },
-  // GitHub Pages를 위한 설정
-  experimental: {
-    payloadExtraction: false,
-  },
-  // 빌드 설정
-  nitro: {
-    preset: 'github_pages',
   },
 });
